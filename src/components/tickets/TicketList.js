3d4fef7cae812e68ea2f5758338468b53285f7cb
Sefora Rodriguez <seforarodriguez13@gmail.com>
2022-08-29 14:19:52 -0400

    ticket list is showing

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -1,4 +1,31 @@
-export const TicketList = () => {
-    return <h2>List of Tickets</h2>
-}
+import { useEffect, useState } from "react"
 
+export const TicketList = () => {
+    const [tickets, setTickets] = useState([])
+
+    useEffect(
+        () => {
+            fetch(' http://localhost:8088/serviceTickets')
+            .then(response => response.json())
+            .then((ticketArray) => {
+                setTickets(ticketArray)
+            })    
+        },
+        [] // When this array is empty, you are observing initial component state
+    )
+    return <>
+    <h2>List of Tickets</h2>
+        <article className="tickets">
+        {
+            tickets.map(ticket => {
+                return <>
+                    <section className="ticket">
+                        <header>{ticket.description}</header>
+                        <footer>Emergency: {ticket.emergency ? "🧨": "No" }</footer>
+                    </section>
+                </>
+            })
+        }
+
+    </article></>
+}
